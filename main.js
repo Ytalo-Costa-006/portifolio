d81c4e68337d02ffb2c41f83f29ca984894f8fac
Ytalo Costa <143295531+Ytalo-Costa-006@users.noreply.github.com>
2023-10-26 21:20:40 -0300

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // ou forma abrevida
 // let myvariable = "bob"
 const myImage = document.querySelector("img.img-codigo")
-let currentImage = "images/gif-do-codigo-leve.gif"
+let currentImage = "images/gif-do-codigo.gif"
 
 
 let v1 = "Toque na no gif para trocar para uma imagem"
@@ -14,7 +14,7 @@ myImage.addEventListener("click",()=>{
 	console.log("clicou")
 	
 	if (currentImage === "./print-codigo.png") {
-		myImage.src = currentImage = "./gif-do-codigo-leve.gif"
+		myImage.src = currentImage = "./gif-do-codigo.gif"
 	} else {
 		myImage.src = currentImage = "./print-codigo.png"
 	}
